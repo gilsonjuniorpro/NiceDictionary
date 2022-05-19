881eba9fc5404aa8c9569e82887d3e21307f2f5f
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-19 19:54:22 -0400

    create entity

---
+++ app/src/main/java/nicedictionary/ca/featuredictionary/data/local/entity/WordInfoEntity.kt
@@ -0,0 +1,25 @@
+package nicedictionary.ca.featuredictionary.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import nicedictionary.ca.featuredictionary.domain.model.Meaning
+import nicedictionary.ca.featuredictionary.domain.model.WordInfo
+
+@Entity
+data class WordInfoEntity(
+    val word: String,
+    val phonetic: String,
+    val sourceUrls: List<String>,
+    val meanings: List<Meaning>,
+    @PrimaryKey val id: Int? = null
+) {
+    fun toWordInfo(): WordInfo {
+        return WordInfo(
+            word = word,
+            phonetic = phonetic,
+            sourceUrls = sourceUrls,
+            meanings = meanings
+        )
+    }
+}
+
