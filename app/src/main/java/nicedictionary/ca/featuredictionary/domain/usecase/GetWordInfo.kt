d47ca00a0dc91a412dec65cf61df392f9d58df9e
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-21 19:59:57 -0400

    create GetWordInfo

---
+++ app/src/main/java/nicedictionary/ca/featuredictionary/domain/usecase/GetWordInfo.kt
@@ -0,0 +1,19 @@
+package nicedictionary.ca.featuredictionary.domain.usecase
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import nicedictionary.ca.core.util.Resource
+import nicedictionary.ca.featuredictionary.domain.model.WordInfo
+import nicedictionary.ca.featuredictionary.domain.repository.WordInfoRepository
+
+class GetWordInfo(
+    private val repository: WordInfoRepository
+) {
+
+    override fun invoke(word: String): Flow<Resource<List<WordInfo>>> {
+        if(word.isBlank()) {
+            return flow {  }
+        }
+        return repository.getWordInfo(word = word)
+    }
+}
